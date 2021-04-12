a2f51d040bfd10013e66fa8c90a5c454e352331b
OlegLeva <oleg.levitskiy@gmail.com>
2021-04-12 15:26:08 +0300

    templates

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,7 +3,4 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', index),
-    path('test/', test),
-    path('pohuy/', pohuy)
-
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    print((request))
-    return HttpResponse('Hello world')
-
-
-def test(request):
-    return HttpResponse('<h1>Test string</h1>')
-
-def pohuy(request):
-    return HttpResponse('<h3>А мне POHUY</h3>')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', context)
